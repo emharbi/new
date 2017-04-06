505a751e1563f8cb228bd8d2fc01d806029557fd
emharbi <emharbi@hotmail.com>
2017-04-06 00:05:44 -0400

    4th

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,18 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
+
   def new
   @article = Article.new
 
 
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -13,6 +22,16 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article)
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Successfully Updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
 
